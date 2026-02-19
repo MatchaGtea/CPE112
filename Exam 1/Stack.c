b549489ff592beacb2aa659a20af314456092c8e
MatchaGtea <punpannatorn957@gmail.com>
2026-02-20 01:04:11 +0700

    hello

---
+++ Exam 1/Stack.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 20
+//stack : pop push peek ini
+
+struct Stack
+{
+    char arr[MAX];
+    int i;
+};
+
+
+void initilize(struct Stack*){
+    ajskldfj
+}
