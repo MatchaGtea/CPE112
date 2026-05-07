640634dfe144ff98663ec6726f0c24f02b1e536e
Matcha <punpannatorn957@gmail.com>
2026-05-07 14:33:55 +0700

    before exam

--- Learn/Reminder.c
+++ Learn/Reminder.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct node{
     int key;
     struct node *left;
     struct node *right;
-} Node;
+}Node;
 
-Node* insert(Node* root, int key) {
-    if (root == NULL) {
-        Node* newNode = (struct node*)malloc(sizeof(struct node));
+Node *insert(Node* root, int key){
+    if(root == NULL){
+        Node *newNode = (struct node*)malloc(sizeof(struct node));
         newNode->key = key;
         newNode->left = newNode->right = NULL;
         return newNode;
     }
 
-    if (key < root->key) {
-        root->left = insert(root->left, key);
-    } else {
+    if(key < root->key){
+        root->left = insert(root->left , key);
+    } else{
         root->right = insert(root->right, key);
     }
     return root;
 }
 
-void inorder(Node* root) {
-    if (root == NULL) {
-        return;
-    }
-
+void inorder(Node *root){
+    if(root == NULL) return;
     inorder(root->left);
     printf("%d ", root->key);
     inorder(root->right);
 }
 
-void solve(int n) {
-    Node* root = NULL;
+void solve(int n){
+    Node *root = NULL;
     int value;
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &value);
+    for(int i = 0; i<n ; i++){
+        scanf("%d",&value);
         root = insert(root, value);
     }
 
@@ -47,7 +44,7 @@ void solve(int n) {
 
 int main() {
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     solve(n);
     return 0;
 }
